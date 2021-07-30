cb02bb580631ac56260f3da2ea99f5548310c443
Ken Cheung <wasd0109.dev@gmail.com>
2021-07-30 20:01:18 +0900

    HomeScreen basic completed

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
-import { List, Provider } from "react-native-paper";
+import { List, Divider, Provider } from "react-native-paper";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 
 const HomeScreen = () => {
   const todoList = useSelector((state: RootState) => state.todo.todoList);
+
   return (
     <FlatList
       data={todoList}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <List.Item title={item.title} />}
+      renderItem={({ item }) => {
+        return (
+          <>
+            <List.Item title={item.title} />
+            <Divider />
+          </>
+        );
+      }}
     />
   );
 };
